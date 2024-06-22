92b23135db40b8d64bfe9eb5aee2b95651a83163
Bugi Goertz <bugi@joboland.de>
2024-06-22 09:36:49 +0200

    Refined the Start Screen
      - moved text around
      - made a too fast starry animation
      - color selection

--- scenes/start.lua
+++ scenes/start.lua
@@ -3,14 +3,28 @@ StartScene = {
   name = 'start',
 
   draw = function()
-    cls(1)
+    cls(0)
+    -- ceiling
+    ceiling = 78
+    circfill(64, 68, ceiling, 1)
+    rectfill(0,64,127,127,0)
+    camera(-64, -64)
+    for _=1,10 do
+      local x = rnd(2 * ceiling) - ceiling
+      local y = rnd(sqrt(ceiling*ceiling - x^2))
+      pset(x , -y,7)
+    end
+    camera()
+    -- title
     printc("picorocket",30,7)
-    printc("a game of superlatives",56,5)
-    line(32,64,96,64,5)
-    printc("- build rockets",68,5)
-    printc("- explore a solar system",76,5)
-    printc("- the universe in your pocket",84,5)
-    printc("have fun / press ❎ to start",106,5)
+    printc("a game of superlatives",38,3)
+    line(0,64,127,64,7 )
+    -- bullets
+    printc("- build a rockets",70,3)
+    printc("- explore a solar system",78,3)
+    printc("put the universe in your pocket",92,3)
+    -- instructions
+    printc("press ❎ to start",109,9)
   end,
 
   init = function()
