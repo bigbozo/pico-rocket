141d351ec670365fb015dc3ee2035eaeaa571c59
Bugi Goertz <bugi@joboland.de>
2024-06-24 22:49:55 +0200

    Planet-Screen extended
    Added different elements to the Planet-Screen
    
      - Legend
        - Distance to sun
        - Size
        - Mass
    
      - Graphic Presentation
        - Display of the moons in size relation

--- scenes/planet.lua
+++ scenes/planet.lua
@@ -1,11 +1,29 @@
 PlanetScene = {
 
   draw= function()
-    cls()
-    camera(0,0)
+
     planet=planets[player.planet]
-    circfill(64,64,planet.size * 4,planet.color)
-    printc(planet.name, 4, 7)
+
+    cls(1)
+    camera(0,0)
+
+    -- draw the planet
+    circfill(0,127 - planet.size * 4,planet.size * 4,planet.color)
+
+    -- print the planet datasheet
+    printr(planet.name, 4, 4)
+    printr("distance " .. planet.r .. "000 pc",12,7)
+    printr("size " .. planet.size .. "0 pc", 20,7)
+    printr("mass " .. planet.size .. "00 t",  28, 7)
+
+    -- draw the moons of the planet
+    if (planet.children) then
+      local dx=planet.size * 4
+      for moon in all(planet.children) do
+       circfill(dx + moon.size * 4 + 4, 127-planet.size*4, moon.size * 4, moon.color)
+        dx = dx + moon.size * 8 + 4
+      end
+    end
 
   end,
 
