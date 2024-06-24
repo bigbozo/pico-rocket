3fe6eaee6dd123e34ad8334872b9ec2598883ee8
Bugi Goertz <bugi@joboland.de>
2024-06-24 22:31:49 +0200

    Add mass property to celestial objects

--- objects.lua
+++ objects.lua
@@ -11,29 +11,30 @@ sun = {
   y=0,
   size=20,
   color=10,
+  mass = 32000,
   children = {
-    { r=50, speed = 17, color=9, size=5, name="picorino"},
-    { r=80, speed = 7, color =11, size=8, name="alpico",
+    { r=50, mass=10, speed = 17, color=9, size=5, name="picorino"},
+    { r=80, mass=40, speed = 7, color =11, size=8, name="alpico",
       children = {
-        {r=15, speed=11, size=3, color=15, name="mambuz"}
+        {r=15, mass=5, speed=11, size=3, color=15, name="mambuz"}
       }
     },
-    { r=140, speed = 5, color =8, size=10, name="portico",
+    { r=140, mass=50, speed = 5, color =8, size=10, name="portico",
       children={
-        {r=20, speed=11, size=4, color=15, name="kerbuz"},
-        {r=30, speed=13, size=3, color=15, name="wambuz"}
+        {r=20, mass=8, speed=11, size=4, color=15, name="kerbuz"},
+        {r=30, mass=5, speed=13, size=3, color=15, name="wambuz"}
       }
     },
-    { r=210, speed=3, color=14, size=15, name="coip",
+    { r=210, mass=200, speed=3, color=14, size=15, name="coip",
       children = {
-        {r=25,speed=7, size=6, color=15, name="shabuz"}
+        {r=25,mass=20, speed=7, size=6, color=15, name="shabuz"}
       }
     },
-    { r=300, speed=2, color=13, size=10, name="pocorn"},
-    { r=450, speed=1, color=12,size=18, name="jupico",
+    { r=300, mass=120, speed=2, color=13, size=10, name="pocorn"},
+    { r=450, mass=400, speed=1, color=12,size=18, name="jupico",
       children= {
-        {r=60, speed=13, size=4, color=15, name="nimbuz"},
-        {r=100, speed=11, size=3, color=15, name="chocbuz"}
+        {r=60, mass=10, speed=13, size=4, color=15, name="nimbuz"},
+        {r=100, mass=6, speed=11, size=3, color=15, name="chocbuz"}
       }
     }
   }
