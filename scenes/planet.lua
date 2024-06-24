53a92ba5f33782fb651a4881aa7e79edfc73d214
Bugi Goertz <bugi@joboland.de>
2024-06-24 20:55:31 +0200

    Use printc to print planet name in planet-scene

--- scenes/planet.lua
+++ scenes/planet.lua
@@ -5,7 +5,8 @@ PlanetScene = {
     camera(0,0)
     planet=planets[player.planet]
     circfill(64,64,planet.size * 4,planet.color)
-    print(planet.name, 63 - #planet.name*2,2,7)
+    printc(planet.name, 4, 7)
+
   end,
 
   update = function()
